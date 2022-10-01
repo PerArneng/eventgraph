de219e1fbcea2d1b87245f55951bf2e31393051d
Per Arneng <per.arneng@scalebit.com>
2022-10-01 08:42:11 -0600

    added logging etc

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use clap::Parser;
+use std::path::Path;
+use std::process;
 
-/// Simple program to greet a person
+use log::{debug, error, warn, log_enabled, info, Level};
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// The event file to parse
+    #[arg(short, long)]
+    file: String,
+
 }
 
 fn main() {
-    let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    env_logger::init();
+    debug!("Mary has a little lamb");
+    error!("{}", "Its fleece was white as snow");
+    info!("{:?}", "And every where that Mary went");
+    warn!("{:#?}", "The lamb was sure to go");
+
+    let args = Args::parse();
+    let path = Path::new(&args.file);
+    if !path.exists() {
+        error!("non existing file: {}!", path.display());
+        process::exit(1);
     }
+
+    info!("Hello {}!", path.display());
 }
